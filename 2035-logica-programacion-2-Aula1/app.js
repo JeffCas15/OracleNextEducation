c8e8bf721ca8eaeab85cdf4a944a34a3ee0951be
Jeffer Dev <jefferdev22@gmail.com>
2024-07-13 19:59:16 -0500

    actualización

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,4 +1,7 @@
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0;
+let numeroIntentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
 function asignarTexto(elemento, texto){
     let titulo = document.querySelector(elemento);
@@ -8,14 +11,57 @@ function asignarTexto(elemento, texto){
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
-    console.log(numeroSecreto);
-    console.log(numeroDeUsuario);
-    console.log(numeroSecreto === numeroDeUsuario);
+    
+    if(listaNumerosSorteados.length == numeroMaximo){
+    asignarTexto("p", "ya se sortearon todos los números posibles");
+    }else{
+        if(numeroSecreto === numeroDeUsuario){
+            asignarTexto("p", `Acertaste el número en ${numeroIntentos} ${(numeroIntentos === 1) ? "intento" : "intentos"}`);
+            document.getElementById("reiniciar").removeAttribute("disabled");
+        }else if(numeroSecreto > numeroDeUsuario){
+            asignarTexto("p", "El número es mayor");
+        }else{
+            asignarTexto("p", "El número es menor");
+        }
+    }    
+
+    numeroIntentos++;
+
+    limpiarCaja();
+}
+
+
+function limpiarCaja(){
+    document.querySelector("#valorUsuario").value = "";
 }
 
 function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    
+    if(listaNumerosSorteados.includes(numeroGenerado)){
+        return generarNumeroSecreto(); //recursividad, utilizando una función que se llama a si misma
+    }else{
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
 }
 
-asignarTexto("h1", "Juego del Número secreto");
-asignarTexto("p", "Inserta un número del 1 al 10");
+function reiniciarJuego(){
+    //limpiar caja
+    limpiarCaja();
+    //indicar mensaje de inicio
+    //inicializar el número de intentos
+    //generar el número aleatório
+    condicionesIniciales();
+    //desabbilidar botón de nuevo juego
+    document.getElementById("reiniciar").setAttribute("disabled", "true");
+}
+
+function condicionesIniciales(){
+    asignarTexto("h1", "Juego del Número secreto");
+    asignarTexto("p", `Inserta un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+condicionesIniciales();
